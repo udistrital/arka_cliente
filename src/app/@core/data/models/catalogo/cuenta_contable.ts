1c0afcf63ac1cab53d5f475f66ffbfdcc2fe328f
Miavegai <miguel_9504@hotmail.com>
2019-10-20 15:42:36 -0500

    Se agrega consulta detalle de arbol #138

--- src/app/@core/data/models/catalogo/cuenta_contable.ts
+++ src/app/@core/data/models/catalogo/cuenta_contable.ts
@@ -3,9 +3,9 @@ import { NivelClasificacion } from './nivel_clasificacion';
 export class CuentaContable {
     Id: number;
     Saldo: number;
-    Nombre: string
-    Naturaleza: string
-    Descripcion: string
-    Codigo: string
+    Nombre: string;
+    Naturaleza: string;
+    Descripcion: string;
+    Codigo: string;
     NivelClasificacion: NivelClasificacion;
 }
