17e22b14a6d3c7882e8d62ab59f07dd96bb3e629
Mafe Avendaño <mfavendannom@correo.udistrital.edu.co>
2019-11-19 09:20:41 -0500

    Ajustando rutas a los servicios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -17,7 +17,7 @@ export const environment = {
       PASS: 'desarrollooas2019',
     },
   },
-  DOCUMENTO_SERVICE: 'http://api.planestic.udistrital.edu.co:8094/v1/',
+  DOCUMENTO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v1/',
   CONFIGURACION_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
   NOTIFICACION_SERVICE: 'ws://pruebasapi.intranetoas.udistrital.edu.co:8116/ws',
   CONF_MENU_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/menu_opcion_padre/ArbolMenus/',
