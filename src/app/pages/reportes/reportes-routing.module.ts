d96283a183c4980391f71bcad1aa4be9b30c4058
anndresfelipe29 <afgomezs@correo.udistrital.edu.co>
2020-01-04 18:34:17 -0500

    Se agregan los camios realizdos para el routing del nuevo componente para los reportes de salida #229

--- src/app/pages/reportes/reportes-routing.module.ts
+++ src/app/pages/reportes/reportes-routing.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReportesComponent } from './reportes.component';
 import { RegistroEntradasComponent } from './registro-entradas/registro-entradas.component';
+import { RegistroSalidasComponent } from './registro-salidas/registro-salidas.component';
 
 const routes: Routes = [{
   path: '',
   component: ReportesComponent,
-  children: [{
+  children: [
+    {
     path: 'registro-entradas',
     component: RegistroEntradasComponent,
+  },
+  {
+    path: 'registro-salidas',
+    component: RegistroSalidasComponent,
   }],
 }];
 
--- src/app/pages/reportes/reportes.module.ts
+++ src/app/pages/reportes/reportes.module.ts
@@ -5,10 +5,12 @@ import { ThemeModule } from '../../@theme/theme.module';
 import { ReportesRoutingModule } from './reportes-routing.module';
 import { ReportesComponent } from './reportes.component';
 import { RegistroEntradasComponent } from './registro-entradas/registro-entradas.component';
+//import { RegistroSalidasComponente } from './registro-salidasss/registro-salidas.component';
 import { ToasterModule } from 'angular2-toaster';
+import { RegistroSalidasComponent } from './registro-salidas/registro-salidas.component';
 
 @NgModule({
-  declarations: [ReportesComponent, RegistroEntradasComponent],
+  declarations: [ReportesComponent, RegistroEntradasComponent, RegistroSalidasComponent],
   imports: [
     CommonModule,
     ThemeModule,
