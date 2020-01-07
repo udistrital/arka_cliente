840314ac89529836ef9b09feb67a8da81178039e
anndresfelipe29 <afgomezs@correo.udistrital.edu.co>
2020-01-07 09:08:56 -0500

    lint

--- src/app/pages/bodega-consumo/bodega-consumo-routing.module.ts
+++ src/app/pages/bodega-consumo/bodega-consumo-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {BodegaConsumoComponent} from './bodega-consumo.component'
-import {KardexComponent} from './kardex/kardex.component'
+import {BodegaConsumoComponent} from './bodega-consumo.component';
+import {KardexComponent} from './kardex/kardex.component';
 const routes: Routes = [{
-  path:'',
+  path: '',
   component: BodegaConsumoComponent,
-  children:[
+  children: [
     {
       path: 'kardex',
-      component: KardexComponent
-    }
+      component: KardexComponent,
+    },
   ],
 }];
 
--- src/app/pages/bodega-consumo/kardex/kardex.component.ts
+++ src/app/pages/bodega-consumo/kardex/kardex.component.ts
@@ -9,8 +9,8 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class KardexComponent implements OnInit {
 
-  constructor(private translate: TranslateService) { 
-    
+  constructor(private translate: TranslateService) {
+
   }
 
   sales: any[];
@@ -27,18 +27,18 @@ export class KardexComponent implements OnInit {
           { brand: 'Sharp', lastYearSale: '82%', thisYearSale: '12%', lastYearProfit: '$131,211', thisYearProfit: '$100,214' },
           { brand: 'Panasonic', lastYearSale: '44%', thisYearSale: '45%', lastYearProfit: '$66,442', thisYearProfit: '$53,322' },
           { brand: 'HTC', lastYearSale: '90%', thisYearSale: '56%', lastYearProfit: '$765,442', thisYearProfit: '$296,232' },
-          { brand: 'Toshiba', lastYearSale: '75%', thisYearSale: '54%', lastYearProfit: '$21,212', thisYearProfit: '$12,533' }
+          { brand: 'Toshiba', lastYearSale: '75%', thisYearSale: '54%', lastYearProfit: '$21,212', thisYearProfit: '$12,533' },
       ];
       this.kardex = [
-        { fecha: '02/01/2020', detalle: 'Ocurrio la situación x', saldoInicialUnidad: '20', saldoInicialVUnitario: '$20.000', 
-        saldoInicialVTotal: '$200.000', entradaUnidad:'50', entradaVUnitario:'$30.000' ,entradaVTotal:'$300.000', salidaUnidad:'60', 
-        salidaVUnitario: '$40.000', salidaVTotal: '$500.000', saldoCantidad:'10', saldoValor:'$70.000' },
-        { fecha: '02/01/2020', detalle: 'Ocurrio la situación x', saldoInicialUnidad: '20', saldoInicialVUnitario: '$20.000', 
-        saldoInicialVTotal: '$200.000', entradaUnidad:'50', entradaVUnitario:'$30.000' ,entradaVTotal:'$300.000', salidaUnidad:'60', 
-        salidaVUnitario: '$40.000', salidaVTotal: '$500.000', saldoCantidad:'10', saldoValor:'$70.000' },
-        { fecha: '02/01/2020', detalle: 'Ocurrio la situación x', saldoInicialUnidad: '20', saldoInicialVUnitario: '$20.000', 
-        saldoInicialVTotal: '$200.000', entradaUnidad:'50', entradaVUnitario:'$30.000' ,entradaVTotal:'$300.000', salidaUnidad:'60', 
-        salidaVUnitario: '$40.000', salidaVTotal: '$500.000', saldoCantidad:'10', saldoValor:'$70.000' }
+        { fecha: '02/01/2020', detalle: 'Ocurrio la situación x', saldoInicialUnidad: '20', saldoInicialVUnitario: '$20.000',
+        saldoInicialVTotal: '$200.000', entradaUnidad: '50', entradaVUnitario: '$30.000' , entradaVTotal: '$300.000', salidaUnidad: ' 60',
+        salidaVUnitario: '$40.000', salidaVTotal: '$500.000', saldoCantidad: '10', saldoValor: ' $70.000' },
+        { fecha: '02/01/2020', detalle: 'Ocurrio la situación x', saldoInicialUnidad: '20', saldoInicialVUnitario: '$20.000',
+        saldoInicialVTotal: '$200.000', entradaUnidad: '50', entradaVUnitario: '$30.000' , entradaVTotal: '$300.000', salidaUnidad: '60',
+        salidaVUnitario: '$40.000', salidaVTotal: '$500.000', saldoCantidad: '10', saldoValor: '$70.000' },
+        { fecha: '02/01/2020', detalle: 'Ocurrio la situación x', saldoInicialUnidad: '20', saldoInicialVUnitario: '$20.000',
+        saldoInicialVTotal: '$200.000', entradaUnidad: '50', entradaVUnitario: '$30.000' , entradaVTotal: '$300.000', salidaUnidad: '60',
+        salidaVUnitario: '$40.000', salidaVTotal: '$500.000', saldoCantidad: '10', saldoValor: '$70.000' },
         ];
   }
 
