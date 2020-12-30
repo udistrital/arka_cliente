073e809bf5602e726dbcf5bcae77b97f1bb2576f
Alex Bustos <alexfbp@gmail.com>
2020-12-29 23:54:09 -0500

    feat: #381 L10n

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -47,11 +47,16 @@ export class PagesComponent {
 
     // Mostrar el tiempo de la sesion
     window.setTimeout(() => {
-      this.pUpManager.showInfoToast('La sesion expira en ' + Math.trunc(queda / 1000 / 60) + ' minuto(s)', 10000);
+      this.pUpManager.showInfoToast(
+        this.translate.instant('GLOBAL.notificaciones.t_restante', {MINUTES: Math.trunc(queda / 1000 / 60)}),
+        10000);
     });
     // Registrar la notificación cuando esté por expirar el token
     window.setTimeout(() => {
-      this.pUpManager.showAlert('warning', 'Queda(n) ' + Math.trunc(T_ADV_TIMEOUT / 1000 / 60) +  ' minuto(s). Por favor guarde los cambios que tenga pendientes', 'titulocochon');
+      this.pUpManager.showAlert(
+        'warning',
+        this.translate.instant('GLOBAL.notificaciones.t_restante_alerta', {MINUTES: Math.trunc(T_ADV_TIMEOUT / 1000 / 60)}),
+        this.translate.instant('GLOBAL.notificaciones.t_restante_titulo'));
     }, queda - T_ADV_TIMEOUT);
   }
 
