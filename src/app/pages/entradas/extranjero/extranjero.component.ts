0e608808aef21738b0f71f8e960b41137c602bfa
Alex Bustos <alexfbp@gmail.com>
2021-01-24 22:47:26 -0500

    feat: #431 ng g c extranjero

---
+++ src/app/pages/entradas/extranjero/extranjero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-extranjero',
+  templateUrl: './extranjero.component.html',
+  styleUrls: ['./extranjero.component.scss'],
+})
+export class ExtranjeroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
