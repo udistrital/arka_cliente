fddc852a28ba4aa7f11a218e33a99443d70b7fdf
Fabian Barreto <fabibeto11@hotmail.com>
2021-02-18 14:01:04 -0500

    hotfix: correción por cambio de nombre en el archivo enviroment.ts

--- src/app/pages/entradas/consulta-entrada/consulta-entrada.component.ts
+++ src/app/pages/entradas/consulta-entrada/consulta-entrada.component.ts
@@ -40,7 +40,6 @@ export class ConsultaEntradaComponent implements OnInit {
   Supervisor: any;
   Proveedores: any;
   Proveedor: any;
-  
   constructor(private router: Router, private entradasHelper: EntradaHelper, private translate: TranslateService,
     private nuxeoService: NuxeoService, private documentoService: DocumentoService, private listService: ListService,
     private store: Store<IAppState>, private tercerosHelper: TercerosHelper) {
