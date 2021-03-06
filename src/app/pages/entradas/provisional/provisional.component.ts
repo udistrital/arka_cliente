630ceac684a97da56f418343a87057e37077a5ec
ludwinggsanchez <ludwingsanchezleon@gmail.com>
2021-03-06 03:31:04 -0500

    fix: Undo #507 Entrada provisional

--- src/app/pages/entradas/provisional/provisional.component.ts
+++ src/app/pages/entradas/provisional/provisional.component.ts
@@ -55,7 +55,6 @@ export class ProvisionalComponent implements OnInit {
   @ViewChild('stepper') stepper: NbStepperComponent;
 
   @Input() actaRecibidoId: Number;
-  @Input() movimientoId: Number;
 
   constructor(private router: Router, private entradasHelper: EntradaHelper, private actaRecibidoHelper: ActaRecibidoHelper,
     private pUpManager: PopUpManager, private fb: FormBuilder) {
@@ -278,7 +277,6 @@ export class ProvisionalComponent implements OnInit {
         EstadoMovimientoId: {
           Id: 2, // Movimiento adecuado para registrar una entrada como aprobada
         },
-        Id: this.movimientoId ? this.movimientoId : 0,
         SoporteMovimientoId: 0,
         IdTipoMovimiento: this.tipoEntrada.Id,
       };
