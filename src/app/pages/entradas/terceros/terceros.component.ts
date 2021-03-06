d94964cae319fafe2917e0901f468e7d6badb218
ludwinggsanchez <ludwingsanchezleon@gmail.com>
2021-03-06 02:39:59 -0500

    fix: Bug cuando no hay contratos asociados a la vigencia y tipo de contrato seleccionados.

--- src/app/pages/entradas/terceros/terceros.component.ts
+++ src/app/pages/entradas/terceros/terceros.component.ts
@@ -12,6 +12,7 @@ import { TipoEntrada } from '../../../@core/data/models/entrada/tipo_entrada';
 import { NavigationExtras, Router } from '@angular/router';
 import { NbStepperComponent } from '@nebular/theme';
 import Swal from 'sweetalert2';
+import { isObject } from 'rxjs/internal-compatibility';
 
 @Component({
   selector: 'ngx-terceros',
@@ -91,18 +92,17 @@ export class TercerosComponent implements OnInit {
     this.getVigencia();
   }
 
-  onFacturaSubmit() {}
+  onFacturaSubmit() { }
 
   /**
    * Métodos para cargar los contratos.
    */
   loadContratos(): void {
+    this.contratos = [];
     if (this.opcionTipoContrato !== '' && this.opcionvigencia) {
       this.entradasHelper.getContratos(this.opcionTipoContrato, this.opcionvigencia).subscribe(res => {
         if (res !== null) {
-          while (this.contratos.length > 0) {
-            this.contratos.pop();
-          }
+          if (isObject(res.contratos_suscritos.contrato_suscritos))
           for (const index of Object.keys(res.contratos_suscritos.contrato_suscritos)) {
             const contratoAux = new Contrato;
             contratoAux.NumeroContratoSuscrito = res.contratos_suscritos.contrato_suscritos[index].numero_contrato;
