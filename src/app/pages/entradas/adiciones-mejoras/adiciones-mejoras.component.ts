f88b6aac0f0dfdd1b004271839b61eb620870802
ludwinggsanchez <ludwingsanchezleon@gmail.com>
2021-03-06 02:48:20 -0500

    fix: Bug cuando no hay contratos asociados a la vigencia y tipo de contrato seleccionados.

--- src/app/pages/entradas/adiciones-mejoras/adiciones-mejoras.component.ts
+++ src/app/pages/entradas/adiciones-mejoras/adiciones-mejoras.component.ts
@@ -12,6 +12,7 @@ import { TipoEntrada } from '../../../@core/data/models/entrada/tipo_entrada';
 import { Router, NavigationExtras } from '@angular/router';
 import { NbStepperComponent } from '@nebular/theme';
 import Swal from 'sweetalert2';
+import { isObject } from 'rxjs/internal-compatibility';
 
 @Component({
   selector: 'ngx-adiciones-mejoras',
@@ -98,17 +99,16 @@ export class AdicionesMejorasComponent implements OnInit {
    * Métodos para cargar los contratos.
    */
   loadContratos(): void {
+    this.contratos = [];
     if (this.opcionTipoContrato !== '' && this.opcionvigencia) {
       this.entradasHelper.getContratos(this.opcionTipoContrato, this.opcionvigencia).subscribe(res => {
         if (res !== null) {
-          while (this.contratos.length > 0) {
-            this.contratos.pop();
-          }
-          for (const index of Object.keys(res.contratos_suscritos.contrato_suscritos)) {
-            const contratoAux = new Contrato;
-            contratoAux.NumeroContratoSuscrito = res.contratos_suscritos.contrato_suscritos[index].numero_contrato;
-            this.contratos.push(contratoAux);
-          }
+          if (isObject(res.contratos_suscritos.contrato_suscritos))
+            for (const index of Object.keys(res.contratos_suscritos.contrato_suscritos)) {
+              const contratoAux = new Contrato;
+              contratoAux.NumeroContratoSuscrito = res.contratos_suscritos.contrato_suscritos[index].numero_contrato;
+              this.contratos.push(contratoAux);
+            }
         }
       });
     }
@@ -264,7 +264,7 @@ export class AdicionesMejorasComponent implements OnInit {
         vigencia_contrato: this.contratoForm.value.vigenciaCtrl,
         importacion: this.checked,
         tipo_contrato: this.opcionTipoContrato === '14' ? 'Orden de Servicios' :
-        this.opcionTipoContrato === '15' ? 'Orden de Compra' : '',
+          this.opcionTipoContrato === '15' ? 'Orden de Compra' : '',
       };
       const movimientoAdquisicion = {
         Observacion: this.observacionForm.value.observacionCtrl,
