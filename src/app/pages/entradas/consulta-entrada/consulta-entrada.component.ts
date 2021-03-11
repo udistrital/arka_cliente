909d2188630530e7f50417e2a218da7b4ceb0c6c
CristianDavidSanchez <crdsanchezb@corro.udistrital.edu.co>
2021-03-11 15:29:16 -0500

    feat: Consulta en registro de salidas solucionada, todo el flujo para reposicion es funcional y correcto  #552

--- src/app/pages/entradas/consulta-entrada/consulta-entrada.component.ts
+++ src/app/pages/entradas/consulta-entrada/consulta-entrada.component.ts
@@ -41,8 +41,8 @@ export class ConsultaEntradaComponent implements OnInit {
   Ordenador: any;
   Proveedores: any;
   Proveedor: any;
-  Placa:any;
-  encargado:any;
+  Placa: any;
+  encargado: any;
 
   constructor(
     private router: Router,
@@ -479,14 +479,14 @@ export class ConsultaEntradaComponent implements OnInit {
   }
   loadDetalleReposicion(info) {
     const detalle = JSON.parse(info.Movimiento.Detalle);
-    this.loadEncargadoByPlaca(detalle.placa_id)
+    this.loadEncargadoByPlaca(detalle.placa_id);
     this.entradaEspecifica.ActaRecibidoId = detalle.acta_recibido_id; // ACTA RECIBIDO
     this.entradaEspecifica.Consecutivo = detalle.consecutivo; // CONSECUTIVO
     this.entradaEspecifica.TipoEntradaId.Nombre = info.TipoMovimiento.TipoMovimientoId.Nombre; // TIPO ENTRADA
     this.entradaEspecifica.Observacion = info.Movimiento.Observacion; // OBSERVACIÓN
-    this.Placa=detalle.placa_id;
+    this.Placa = detalle.placa_id;
     this.documentoId = false;
-    this.mostrar=true;
+    this.mostrar = true;
     // console.log(this.Proveedor)
   }
 
@@ -522,7 +522,7 @@ export class ConsultaEntradaComponent implements OnInit {
     });
     this.mostrar = true;
   }
-  loadEncargadoByPlaca(placa:string) {
+  loadEncargadoByPlaca(placa: string) {
     this.entradasHelper.getEncargadoElementoByPlaca(placa).subscribe(res => {
       if (res != null && res !== undefined) {
         this.encargado = res.NombreCompleto;
