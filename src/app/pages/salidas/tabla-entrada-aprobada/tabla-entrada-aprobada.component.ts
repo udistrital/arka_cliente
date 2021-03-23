c62b47c9f92ce74727c935bffda867d4a5c6e728
CristianDavidSanchez <crdsanchezb@corro.udistrital.edu.co>
2021-03-23 12:58:19 -0500

    feat: Arreglo de la tabla en modulo de salidas, de la misma manera que en entradas.

--- src/app/pages/salidas/tabla-entrada-aprobada/tabla-entrada-aprobada.component.ts
+++ src/app/pages/salidas/tabla-entrada-aprobada/tabla-entrada-aprobada.component.ts
@@ -124,7 +124,7 @@ export class TablaEntradaAprobadaComponent implements OnInit {
         TipoEntradaId: {
           title: this.translate.instant('GLOBAL.tipo_entrada'),
           valuePrepareFunction: (value: any) => {
-            return value.Nombre;
+            return value;
           },
           filter: {
             type: 'list',
@@ -137,6 +137,13 @@ export class TablaEntradaAprobadaComponent implements OnInit {
                 { value: 'Reposición', title: 'Reposición' },
                 { value: 'Sobrante', title: 'Sobrante' },
                 { value: 'Terceros', title: 'Terceros' },
+                { value: 'Caja menor', title: 'Caja Menor' },
+                { value: 'Desarrollo interior', title: 'Desarrollo interior' },
+                { value: 'Adiciones y mejoras', title: 'Adiciones y mejoras' },
+                { value: 'Intangibles', title: 'Intangibles' },
+                { value: 'Aprovechamientos', title: 'Aprovechamientos' },
+                { value: 'Compras extranjeras', title: 'Compras extranjeras' },
+                { value: 'Provisional', title: 'Provisional' },
               ],
             },
           },
@@ -157,8 +164,8 @@ export class TablaEntradaAprobadaComponent implements OnInit {
           entrada.ActaRecibidoId = detalle.acta_recibido_id;
           entrada.FechaCreacion = datos.FechaCreacion;
           entrada.Consecutivo = detalle.consecutivo;
-          tipoEntradaAux.Nombre = datos.FormatoTipoMovimientoId.Nombre;
-          entrada.TipoEntradaId = tipoEntradaAux;
+          // tipoEntradaAux.Nombre = datos.FormatoTipoMovimientoId.Nombre;// Innecesario genera conflictos con el filter
+          entrada.TipoEntradaId = datos.FormatoTipoMovimientoId.Nombre;
           this.entradas.push(entrada);
         }
         this.source.load(this.entradas);
