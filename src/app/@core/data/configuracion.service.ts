cfece8072ee9905a64cfde3b3a6a759f764a8773
Alex Bustos <alexfbp@gmail.com>
2021-03-25 00:30:43 -0500

    refactor: (menor) uso del path en configuracion service

--- src/app/@core/data/configuracion.service.ts
+++ src/app/@core/data/configuracion.service.ts
@@ -5,23 +5,32 @@ import { RequestManager } from '../../managers/requestManager';
 @Injectable()
 export class ConfiguracionService {
 
-  constructor(private requestManager: RequestManager) {
-    this.requestManager.setPath('CONFIGURACION_SERVICE');
+  private path: string = 'CONFIGURACION_SERVICE';
+
+  constructor(
+    private requestManager: RequestManager,
+  ) {
   }
 
+  // Funciones CRUD
+
   get(endpoint) {
+    this.requestManager.setPath(this.path);
     return this.requestManager.get(endpoint);
   }
 
   post(endpoint, element) {
+    this.requestManager.setPath(this.path);
     return this.requestManager.post(endpoint, element);
   }
 
   put(endpoint, element) {
+    this.requestManager.setPath(this.path);
     return this.requestManager.put(endpoint, element);
   }
 
   delete(endpoint, element) {
+    this.requestManager.setPath(this.path);
     return this.requestManager.delete(endpoint, element.Id);
   }
 }
