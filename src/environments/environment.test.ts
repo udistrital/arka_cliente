f70f385b46945d6d009027006c7ee8b5095551dc
ludwinggsanchez <ludwingsanchezleon@gmail.com>
2021-04-16 12:23:17 -0500

    lint

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -12,7 +12,7 @@ export const environment = {
       USERNAME: 'desarrollooas',
       PASS: 'desarrollooas2019',
     },
-  },  
+  },
   DOCUMENTO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v1/',
   CONFIGURACION_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
   NOTIFICACION_SERVICE: 'ws://pruebasapi.intranetoas.udistrital.edu.co:8116/ws',
