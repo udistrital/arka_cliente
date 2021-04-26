9de2bea787cbeabeae369d87f0d9b96e10536d90
ludwinggsanchez <ludwingsanchezleon@gmail.com>
2021-04-25 21:18:23 -0500

    feat: #602. Se genera nuevo componente y se configura el routing.

--- src/app/pages/parametros/parametros-routing.module.ts
+++ src/app/pages/parametros/parametros-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TiposBienComponent } from './tipos-bien/tipos-bien.component';
 import { TiposEntradaComponent } from './tipos-entrada/tipos-entrada.component';
 
 const routes: Routes = [
   {
     path: 'tipos-entrada',
     component: TiposEntradaComponent,
+  },
+  {
+    path: 'tipos-bien',
+    component: TiposBienComponent,
   },
 ];
 
--- src/app/pages/parametros/parametros.module.ts
+++ src/app/pages/parametros/parametros.module.ts
@@ -7,9 +7,14 @@ import { ThemeModule } from '../../@theme/theme.module';
 import { ParametrosRoutingModule } from './parametros-routing.module';
 import { ParametrosComponent } from './parametros.component';
 import { TiposEntradaComponent } from './tipos-entrada/tipos-entrada.component';
+import { TiposBienComponent } from './tipos-bien/tipos-bien.component';
 
 @NgModule({
-  declarations: [ParametrosComponent, TiposEntradaComponent],
+  declarations: [
+    ParametrosComponent,
+    TiposEntradaComponent,
+    TiposBienComponent
+  ],
   imports: [
     CommonModule,
     ParametrosRoutingModule,
