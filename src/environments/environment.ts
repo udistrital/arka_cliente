d3ae1fd14244dd361850d2fbaccf618b3d4b1f49
ludwinggsanchez <ludwingsanchezleon@gmail.com>
2021-07-10 20:59:02 -0500

    lint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -50,7 +50,7 @@ export const environment = {
     CONTEXTPATH: 'knowage',
     USER: 'desarrollooas',
     PASSWORD: 'desarrollooas',
-    DOCUMENT_LABEL_ENTRADAS: 'RteEntradaArka'
+    DOCUMENT_LABEL_ENTRADAS: 'RteEntradaArka',
   },
   TOKEN: {
     AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
