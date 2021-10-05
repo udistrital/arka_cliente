6db3cc95cc15afa0531a47c5bffb75e6ed56c000
ludwinggsanchez <ludwingsanchezleon@gmail.com>
2021-10-04 21:27:30 -0500

    feat #784: Autenticacion por google
    Se remueve flujo de autenticacion anterior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,25 @@ import { Component, OnInit } from '@angular/core';
 import { AnalyticsService } from './@core/utils/analytics.service';
 import {TranslateService} from '@ngx-translate/core';
 import 'hammerjs';
+import { Router } from '@angular/router';
+import { ImplicitAutenticationService } from './@core/utils/implicit_autentication.service';
 
 @Component({
   selector: 'ngx-app',
+  styleUrls: ['./app.component.scss'],
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
-  constructor(private analytics: AnalyticsService, private translateService: TranslateService) {
+  constructor(private analytics: AnalyticsService,
+    private translateService: TranslateService,
+    private route: Router,
+    private autenticacion: ImplicitAutenticationService) {
+    this.autenticacion.user$.subscribe((data: any) => {
+      const { user, userService } = data;
+      if (user && userService) {
+        this.route.navigateByUrl('pages');
+      }
+    });
   }
   ngOnInit(): void {
     this.analytics.trackPageViews();
