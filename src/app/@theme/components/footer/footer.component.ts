01712de0e48e5c6b2f82d490405d810de5f3d32a
ludwinggsanchez <ludwingsanchezleon@gmail.com>
2021-10-07 23:12:00 -0500

    feat #803: Ajuste Footer
    - Refactor footer-component: Se crea servicio adicional en caso que por lineamiento se deba consultar esta informacion de un servicio externo

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -1,49 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { FooterService } from '../../../@core/utils/footer.service';
 
 @Component({
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   templateUrl: './footer.component.html',
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
-  universidad: any;
-  normatividad: any;
-  recomendados: any;
-  contactenos: any;
-  final: any;
-  copyright: any;
   social: any;
 
-  constructor(
-    private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer,
-  ) {
+  constructor(private footerService: FooterService) { }
+
+  ngOnInit() {
     this.social = {
-      list: [{
-        title: 'Horario',
-        class: 'fas fa-clock',
-        value: ['Lunes a viernes', '8am a 5pm'],
-      }, {
-        title: 'Nombre',
-        class: 'fas fa-globe-americas',
-        value: ['Sistema Integrado de informática y  Telecomunicaciones '],
-      }, {
-        title: 'Phone',
-        class: 'fas fa-phone',
-        value: ['323 93 00', 'Ext. 1112'],
-      }, {
-        title: 'Direccion',
-        class: 'fas fa-map-marker-alt',
-        value: ['Cra 8 # 40-78', 'Piso 1'],
-      }, {
-        title: 'mail',
-        class: 'fas fa-at',
-        value: ['computo@udistrital.edu.co'],
-      },
-    ],
+      list: [
+        {
+          class: 'fas fa-clock',
+          value: this.footerService.getHorarios(),
+        },
+        {
+          class: 'fas fa-globe-americas',
+          value: this.footerService.getProceso(),
+        },
+        {
+          class: 'fas fa-phone-alt',
+          value: this.footerService.getTelefono(),
+        },
+        {
+          class: 'fas fa-map-marker-alt',
+          value: this.footerService.getDireccion(),
+        },
+        {
+          class: 'fas fa-at',
+          value: this.footerService.getEmail(),
+        },
+      ],
     };
   }
+
 }
