0f021ea6fbdf93071c9cb9234b27e0c43d146b42
Juan David Velez Alvarez <juan.velez@and.gov.co>
2021-11-30 20:40:02 -0500

    Ajustes solicitados al comprobante

--- src/app/pages/entradas/ver-comprobante/ver-comprobante.component.ts
+++ src/app/pages/entradas/ver-comprobante/ver-comprobante.component.ts
@@ -9,12 +9,22 @@ import { Component, Input, OnInit } from '@angular/core';
 
 export class VerComprobanteComponent implements OnInit {
   @Input() transaccion: any;
+  totalCreditos: any; 
+  totalDebitos: any; 
+
   constructor() { }
 
   ngOnInit() {
      console.log("esta es la transaccion: ")  
      console.log(this.transaccion)  
-     console.log(this.transaccion.Movimientos)  
+     console.log("Estos son los movimientos",this.transaccion.movimientos)  
+     this.totalCreditos = 0;
+     this.totalDebitos = 0;
+this.transaccion.movimientos.forEach(obj => { console.log("elk valor",obj.Valor); 
+                                              if (obj.TipoMovimientoId == 344) {this.totalDebitos += obj.Valor}
+                                              if (obj.TipoMovimientoId == 345) {this.totalCreditos += obj.Valor}
+  });
+      console.log("El total del comprobante", this.totalCreditos, this.totalDebitos);
   }
 
 }
