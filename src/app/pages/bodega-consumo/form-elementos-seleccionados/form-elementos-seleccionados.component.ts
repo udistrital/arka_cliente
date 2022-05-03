20d7c6e2ceed27bc21fedae1054784300467cde1
Ludwing Sanchez <ludwingsanchezleon@gmail.com>
2022-05-02 23:43:04 -0500

    feat udistrital/arka_cliente#854: Hallazgos
    - Se incluyen consecutivos en solicitudes de bodega de consumo.
    - Los elementos y clases del catálogo se muestra el código+nombre.
    - Se cambia el controlador para traer la lista de solicitudes.
    - Se cambia el controlador para hacer el post de una solicitud.

--- src/app/pages/bodega-consumo/form-elementos-seleccionados/form-elementos-seleccionados.component.ts
+++ src/app/pages/bodega-consumo/form-elementos-seleccionados/form-elementos-seleccionados.component.ts
@@ -1,51 +1,27 @@
-import { Component, OnInit, ViewChild, ViewChildren, QueryList, Input, Output, EventEmitter } from '@angular/core';
-import { Subscription, combineLatest, Observable } from 'rxjs';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
-import { NuxeoService } from '../../../@core/utils/nuxeo.service';
-
-import { MatTable } from '@angular/material';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import 'hammerjs';
 import { ActaRecibidoHelper } from '../../../helpers/acta_recibido/actaRecibidoHelper';
-import { ActaRecibido } from '../../../@core/data/models/acta_recibido/acta_recibido';
-import { Elemento, Impuesto } from '../../../@core/data/models/acta_recibido/elemento';
-import { TipoBien } from '../../../@core/data/models/acta_recibido/tipo_bien';
-import { SoporteActa, Ubicacion } from '../../../@core/data/models/acta_recibido/soporte_acta';
-import { Proveedor } from '../../../@core/data/models/acta_recibido/Proveedor';
-import { EstadoActa } from '../../../@core/data/models/acta_recibido/estado_acta';
-import { EstadoElemento } from '../../../@core/data/models/acta_recibido/estado_elemento';
-import { HistoricoActa } from '../../../@core/data/models/acta_recibido/historico_acta';
 import Swal from 'sweetalert2';
-import { ToasterService, ToasterConfig, Toast, BodyOutputType } from 'angular2-toaster';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
-import { Unidad } from '../../../@core/data/models/acta_recibido/unidades';
 import { CompleterService, CompleterData } from 'ng2-completer';
-import { HttpLoaderFactory } from '../../../app.module';
-import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Store } from '@ngrx/store';
 import { IAppState } from '../../../@core/store/app.state';
 import { ListService } from '../../../@core/store/services/list.service';
 import { PopUpManager } from '../../../managers/popUpManager';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { DocumentoService } from '../../../@core/data/documento.service';
 import { LocalDataSource } from 'ng2-smart-table';
 import { BodegaConsumoHelper } from '../../../helpers/bodega_consumo/bodegaConsumoHelper';
 import { UserService } from '../../../@core/data/users.service';
-
 
 @Component({
   selector: 'ngx-form-elementos-seleccionados',
   templateUrl: './form-elementos-seleccionados.component.html',
   styleUrls: ['./form-elementos-seleccionados.component.scss'],
 })
+
 export class FormElementosSeleccionadosComponent implements OnInit {
 
-
-  searchStr: string;
-  searchStr2: Array<string>;
-  searchStr3: string;
-  dataService: CompleterData;
-  dataService2: CompleterData;
   dataService3: CompleterData;
   Proveedores: any;
   Dependencias: any;
@@ -56,6 +32,7 @@ export class FormElementosSeleccionadosComponent implements OnInit {
   settings2: any;
   source2: LocalDataSource;
   detalle2: boolean;
+  consecutivo: number;
 
   @Input('Datos')
   set name(datos_seleccionados: any) {
@@ -66,30 +43,21 @@ export class FormElementosSeleccionadosComponent implements OnInit {
   constructor(
     private translate: TranslateService,
     private router: Router,
-    private route: ActivatedRoute,
     private fb: FormBuilder,
     private Actas_Recibido: ActaRecibidoHelper,
-    private toasterService: ToasterService,
     private completerService: CompleterService,
     private store: Store<IAppState>,
     private listService: ListService,
-    private pUpManager: PopUpManager,
-    private sanitization: DomSanitizer,
-    private nuxeoService: NuxeoService,
-    private documentoService: DocumentoService,
     private bodegaConsumoHelper: BodegaConsumoHelper,
     private userService: UserService,
-
-
+    private pUpManager: PopUpManager,
   ) {
     this.translate.onLangChange.subscribe((event: LangChangeEvent) => { // Live reload
     });
     this.source2 = new LocalDataSource();
-    // this.listService.findProveedores();
     this.listService.findDependencias();
     this.listService.findSedes();
     this.loadTablaSettings();
-    // this.listService.findUbicaciones();
     this.loadLists();
 
   }
@@ -100,19 +68,14 @@ export class FormElementosSeleccionadosComponent implements OnInit {
   public loadLists() {
     this.store.select((state) => state).subscribe(
       (list) => {
-        // this.Proveedores = list.listProveedores[0];
         this.Dependencias = list.listDependencias[0];
-        // this.Ubicaciones = list.listUbicaciones[0];
         this.Sedes = list.listSedes[0];
-        // this.dataService2 = this.completerService.local(this.Proveedores, 'compuesto', 'compuesto');
         this.dataService3 = this.completerService.local(this.Dependencias, 'Nombre', 'Nombre');
-        // this.dataService = this.completerService.local(this.Ubicaciones, 'Nombre', 'Nombre');
       },
     );
   }
   get Formulario(): FormGroup {
     return this.fb.group({
-
       Cantidad: ['', Validators.required],
       Sede: ['', Validators.required],
       Dependencia: ['', Validators.required],
@@ -141,7 +104,7 @@ export class FormElementosSeleccionadosComponent implements OnInit {
     }
   }
 
-  onSubmit() {
+  onSeleccionarElemento() {
     const form = this.form_salida.value;
     // console.log(form);
     // console.log(this.Datos);
@@ -167,15 +130,18 @@ export class FormElementosSeleccionadosComponent implements OnInit {
     }
 
   }
-  onSubmit2() {
+
+  onSubmit() {
+    if (!this.checkTercero()) {
+      return;
+    }
+
     this.source2.getElements().then((res: any) => {
-      // console.log(res);
       const detalle_solicitud = [];
       for (const elements of res) {
-
         const elemento = {
           Ubicacion: elements.Ubicacion.Id,
-          ElementoActa: elements.ElementoCatalogoId.Id,
+          ElementoCatalogoId: elements.ElementoCatalogoId.Id,
           Cantidad: elements.Cantidad,
         };
         detalle_solicitud.push(elemento);
@@ -184,29 +150,25 @@ export class FormElementosSeleccionadosComponent implements OnInit {
         Funcionario: this.userService.getPersonaId(),
         Elementos: detalle_solicitud,
       };
-      // console.log(solicitud);
-      const movimiento = {
-        Observacion: 'Solicitud de elementos para Bodega de Consumo',
-        Detalle: JSON.stringify(solicitud),
-        Activo: true,
-        MovimientoPadreId: null, // parseFloat(this.entradaId),
-        FormatoTipoMovimientoId: {
-          Id: 8,
-        },
-        EstadoMovimientoId: {
-          Id: 5,
-        },
-      };
-      this.bodegaConsumoHelper.postSolicitud(movimiento).subscribe((res_: any) => {
-        const opt: any = {
-          title: 'Solicitud No ' + res_.Id,
-          text: 'Se ha registrado la solicitud de los elementos relacionados',
-          type: 'success',
-        };
-        (Swal as any).fire(opt);
-        this.router.navigate(['/pages/bodega_consumo/consulta_solicitud']);
+
+      this.bodegaConsumoHelper.postSolicitud(solicitud).subscribe((res_: any) => {
+        if (res_.Id) {
+          this.consecutivo = res_.Detalle ? JSON.parse(res_.Detalle).Consecutivo : '';
+          this.pUpManager.showAlertWithOptions(this.optionsRegistro);
+          this.router.navigate(['/pages/bodega_consumo/consulta_solicitud']);
+        }
       });
     });
+  }
+
+  private checkTercero(): boolean {
+    if (!this.userService.getPersonaId()) {
+      this.pUpManager.showAlertWithOptions(this.optionsErrorTercero);
+      return false;
+    }
+
+    return true;
+
   }
 
   loadTablaSettings() {
@@ -226,31 +188,14 @@ export class FormElementosSeleccionadosComponent implements OnInit {
         ],
       },
       columns: {
-        Nombre: {
+        CodigoElemento: {
+          title: this.translate.instant('GLOBAL.codigo'),
+        },
+        NombreElemento: {
           title: this.translate.instant('GLOBAL.Nombre'),
         },
-        ElementoCatalogoId: {
+        Descripcion: {
           title: this.translate.instant('GLOBAL.Descripcion'),
-          valuePrepareFunction: (value: any) => {
-            if (value !== null) {
-              return value.Descripcion;
-            } else {
-              return '';
-            }
-          },
-          filterFunction: (cell?: any, search?: string): boolean => {
-            // console.log(cell);
-            // console.log(search);
-            if (Object.keys(cell).length !== 0) {
-              if (cell.Descripcion.indexOf(search) > -1) {
-                return true;
-              } else {
-                return false;
-              }
-            } else {
-              return false;
-            }
-          },
         },
         Cantidad: {
           title: this.translate.instant('GLOBAL.cantidad'),
@@ -344,10 +289,30 @@ export class FormElementosSeleccionadosComponent implements OnInit {
       this.source2.update(res, elemento);
 
     }).catch(() => {
+      elemento.NombreElemento = elemento.ElementoCatalogoId.Nombre;
+      elemento.CodigoElemento = elemento.ElementoCatalogoId.Codigo;
+      elemento.Descripcion = elemento.ElementoCatalogoId.Descripcion;
       this.source2.add(elemento);
       this.source2.refresh();
     });
     this.source2.refresh();
     this.detalle2 = true;
   }
+
+  get optionsRegistro() {
+    return {
+      type: 'success',
+      title: this.translate.instant('GLOBAL.salidas.exito_registro_titulo'),
+      text: this.translate.instant('GLOBAL.salidas.exito_registro_texto', { CONSECUTIVO: this.consecutivo }),
+    };
+  }
+
+  get optionsErrorTercero() {
+    return {
+      type: 'error',
+      title: this.translate.instant('GLOBAL.error'),
+      text: this.translate.instant('GLOBAL.Errores.errorTercero'),
+    };
+  }
+
 }
