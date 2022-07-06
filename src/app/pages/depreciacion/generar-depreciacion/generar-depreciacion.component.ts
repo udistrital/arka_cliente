444a1e0431dc42b75a8175e2360a3e9f3534b1e2
Ludwing Sanchez <ludwingsanchezleon@gmail.com>
2022-07-06 03:38:12 -0500

    feat #887: Cierres
    - Se verifica el estado de los cierres y la edición de las cuentas contables antes de realizar un ajuste automático.
    - Se ajusta el flujo del rechazo de un cierre a través del api arka_mid.

--- src/app/pages/depreciacion/generar-depreciacion/generar-depreciacion.component.ts
+++ src/app/pages/depreciacion/generar-depreciacion/generar-depreciacion.component.ts
@@ -5,7 +5,6 @@ import { DepreciacionHelper } from '../../../helpers/movimientos/depreciacionHel
 import { PopUpManager } from '../../../managers/popUpManager';
 import Swal from 'sweetalert2';
 import { EntradaHelper } from '../../../helpers/entradas/entradaHelper';
-import { EstadoMovimiento } from '../../../@core/data/models/entrada/entrada';
 
 @Component({
   selector: 'ngx-generar-depreciacion',
@@ -22,11 +21,9 @@ export class GenerarDepreciacionComponent implements OnInit {
   loading: boolean;
   showForm: boolean;
   action: string;
-  movimiento: any;
   rechazo: string = '';
   submitted: boolean;
   consecutivo: string = '';
-  estadosMovimiento: Array<EstadoMovimiento>;
   @Output() accion: EventEmitter<boolean> = new EventEmitter<boolean>();
   @Input() modoCrud: string; // create | get | review | update
   @Input() depreciacionId: number = 0;
@@ -50,7 +47,6 @@ export class GenerarDepreciacionComponent implements OnInit {
       this.refDate.setDate(this.refDate.getDate() + 1);
     }
     this.buildForm();
-    this.loadEstados();
     this.title = this.translate.instant('GLOBAL.' + this.tipo + '.' + this.modoCrud + '.title');
     this.subtitle = this.translate.instant('GLOBAL.' + this.tipo + '.' + this.modoCrud + '.subtitle');
     this.action = this.translate.instant('GLOBAL.' + this.tipo + '.' + this.modoCrud + '.accion');
@@ -64,7 +60,6 @@ export class GenerarDepreciacionComponent implements OnInit {
   private getDepreciacion(depreciacionId: number) {
     this.depreciacionHelper.getDepreciacion(depreciacionId).subscribe(res => {
       if (res) {
-        this.movimiento = res.Movimiento;
         this.consecutivo = res.Movimiento && res.Movimiento.Detalle ? JSON.parse(res.Movimiento.Detalle).Consecutivo : '';
         this.fillForm(res.Movimiento);
         this.trContable = {
@@ -72,14 +67,6 @@ export class GenerarDepreciacionComponent implements OnInit {
           movimientos: res.TransaccionContable.movimientos,
           concepto: res.TransaccionContable.Concepto,
         };
-      }
-    });
-  }
-
-  private loadEstados() {
-    this.entradasHelper.getEstadosMovimiento().toPromise().then(res => {
-      if (res.length > 0) {
-        this.estadosMovimiento = res;
       }
     });
   }
@@ -146,28 +133,14 @@ export class GenerarDepreciacionComponent implements OnInit {
             text: this.translate.instant('GLOBAL.' + this.tipo + '.review.confrmRechazoTtx'),
           },
         ]).then((result2) => {
-          if (result2.value) {
+          if (result2.value && result2.value.length && result2.value[0].length) {
             const rechazo = result2.value[0];
-            const detalle = JSON.parse(this.movimiento.Detalle);
-            detalle.RazonRechazo = rechazo;
-            this.movimiento.EstadoMovimientoId = this.estadosMovimiento.find(st => st.Nombre === 'Cierre Rechazado');
-            this.movimiento.Detalle = JSON.stringify(detalle);
-            this.updateDepreciacion();
+            this.formDepreciacion.get('razon').setValue(rechazo);
+            this.submitForm(true);
           }
         });
 
       }
-    });
-  }
-
-  // Revision
-  private updateDepreciacion() {
-    this.loading = true;
-    this.entradasHelper.putMovimiento(this.movimiento).toPromise().then((res: any) => {
-      this.loading = false;
-      this.submitted = true;
-      this.accion.emit(true);
-      this.alertSuccess(this.trContable, true);
     });
   }
 
@@ -186,13 +159,12 @@ export class GenerarDepreciacionComponent implements OnInit {
       cancelButtonText: this.translate.instant('GLOBAL.no'),
     }).then((result) => {
       if (result.value) {
-        this.submitForm();
+        this.submitForm(false);
       }
     });
   }
 
-  // Create/Update
-  public submitForm() {
+  public submitForm(Rechazar: boolean) {
     this.trContable = undefined;
     this.loading = true;
     const date = new Date(this.formDepreciacion.controls.fecha.value);
@@ -204,17 +176,21 @@ export class GenerarDepreciacionComponent implements OnInit {
       FechaCorte,
       Observaciones: obs,
       RazonRechazo: this.formDepreciacion.controls.razon.value,
+      Rechazar,
     };
 
-    if (this.modoCrud === 'create' || this.modoCrud === 'update') {
+    if (this.modoCrud === 'create' || this.modoCrud === 'update' || Rechazar) {
       this.depreciacionHelper.postSolicitud(data).subscribe((res: any) => {
         this.loading = false;
-        if (!res.Error && res.TransaccionContable && !res.TransaccionContable.movimientos) {
-          this.pUpManager.showErrorAlert(this.translate.instant('GLOBAL.' + this.tipo + '.errorVacio'));
-        } else if (res.Error !== '') {
+        if (res.Error !== '') {
+          this.pUpManager.showErrorAlert(res.Error);
+        } else if (Rechazar && res.Movimiento.Id) {
+          this.loading = false;
           this.submitted = true;
-          this.showForm = false;
-          this.pUpManager.showErrorAlert(res.TransaccionContable.errorTransaccion);
+          this.accion.emit(true);
+          this.alertSuccess(this.trContable, true);
+        } else if (res.TransaccionContable && !res.TransaccionContable.movimientos) {
+          this.pUpManager.showErrorAlert(this.translate.instant('GLOBAL.' + this.tipo + '.errorVacio'));
         } else {
           this.trContable = {
             rechazo: '',
@@ -236,9 +212,8 @@ export class GenerarDepreciacionComponent implements OnInit {
         } else if (res.Error !== '') {
           this.submitted = true;
           this.showForm = false;
-          this.pUpManager.showErrorAlert(res.TransaccionContable.errorTransaccion);
+          this.pUpManager.showErrorAlert(res.Error);
         } else {
-
           this.trContable = {
             rechazo: '',
             movimientos: res.TransaccionContable.movimientos,
