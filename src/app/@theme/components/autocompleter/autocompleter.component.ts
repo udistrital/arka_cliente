532625a1793157b280c125ec56bc5910be2f24b7
Alex F. Bustos P <alexfbp@gmail.com>
2022-07-22 14:49:10 -0500

    feat: ng g component autocompleter

---
+++ src/app/@theme/components/autocompleter/autocompleter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-autocompleter',
+  templateUrl: './autocompleter.component.html',
+  styleUrls: ['./autocompleter.component.scss'],
+})
+export class AutocompleterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
