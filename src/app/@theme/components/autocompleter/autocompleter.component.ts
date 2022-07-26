fb5d61bfcd16a5d1025dade1fbd9a690146a2747
Alex F. Bustos P <alexfbp@gmail.com>
2022-07-26 12:05:43 -0500

    feat: implementado autocompleter con datos estáticos

--- src/app/@theme/components/autocompleter/autocompleter.component.ts
+++ src/app/@theme/components/autocompleter/autocompleter.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { debounceTime, map, startWith } from 'rxjs/operators';
+
+interface AutocompleterOption {
+  value: number | string;
+  name: string;
+}
 
 @Component({
   selector: 'ngx-autocompleter',
   templateUrl: './autocompleter.component.html',
   styleUrls: ['./autocompleter.component.scss'],
 })
-export class AutocompleterComponent implements OnInit {
+class AutocompleterComponent implements OnInit {
+
+  filteredOptions: Observable<AutocompleterOption[]>;
+
+  @Input() control: AbstractControl;
+  @Input() options: AutocompleterOption[];
+
+  @Input() value: AutocompleterOption;
+  @Output() valueChanges = new EventEmitter<AutocompleterOption>();
 
   constructor() { }
 
   ngOnInit() {
+    this.filteredOptions = this.control.valueChanges.pipe(
+      debounceTime(250),
+      startWith(''),
+      map(value => typeof value === 'string' ? value : value.text),
+      map(name => name ? this.filter(name) : this.options.slice()),
+    );
+  }
+
+  private filter(name: string): AutocompleterOption[] {
+    return this.options
+    .filter(val => val.name.toLowerCase().includes(name.toLowerCase()))
+  }
+
+  showOption(option: AutocompleterOption): string {
+    return (option && option.name)? option.name : '';
   }
 
 }
+
+export {
+  AutocompleterOption,
+  AutocompleterComponent,
+};
