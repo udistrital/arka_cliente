03cbac4103b20b26d8b4ccba44ea94d731dc4aa5
Ludwing Sanchez <ludwingsanchezleon@gmail.com>
2022-11-23 14:16:17 -0500

    feat #1043: Pruebas de carga y seguridad
    - Post de documento a través de gestor documental en módulo de entradas.

--- src/app/pages/entradas/elaboracion-propia/elaboracion-propia.component.ts
+++ src/app/pages/entradas/elaboracion-propia/elaboracion-propia.component.ts
@@ -1,24 +1,22 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
-import { OrdenadorGasto } from '../../../@core/data/models/entrada/ordenador_gasto';
 import { EntradaHelper } from '../../../helpers/entradas/entradaHelper';
 import { PopUpManager } from '../../../managers/popUpManager';
-import { NuxeoService } from '../../../@core/utils/nuxeo.service';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { DocumentoService } from '../../../@core/data/documento.service';
 import { TranslateService } from '@ngx-translate/core';
 import { TransaccionEntrada } from '../../../@core/data/models/entrada/entrada';
-import { TercerosHelper } from '../../../helpers/terceros/tercerosHelper';
-import { Ordenador, Supervisor, TerceroCriterioJefe, TerceroCriterioPlanta } from '../../../@core/data/models/terceros_criterio';
-import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
+import { Ordenador, Supervisor } from '../../../@core/data/models/terceros_criterio';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { GestorDocumentalService } from '../../../helpers/gestor_documental/gestorDocumentalHelper';
+import { ImplicitAutenticationService } from '../../../@core/utils/implicit_autentication.service';
+import { CommonEntradas } from '../CommonEntradas';
 
 @Component({
   selector: 'ngx-elaboracion-propia',
   templateUrl: './elaboracion-propia.component.html',
   styleUrls: ['./elaboracion-propia.component.scss'],
 })
-
 export class ElaboracionPropiaComponent implements OnInit {
 
   soporteForm: FormGroup;
@@ -26,55 +24,41 @@ export class ElaboracionPropiaComponent implements OnInit {
   ordenadorForm: FormGroup;
   supervisorForm: FormGroup;
   flag = true;
-  dependenciaSupervisor: String;
 
   Supervisores: Supervisor[];
   supervisoresFiltrados: Observable<Supervisor[]>;
   Ordenadores: Ordenador[];
   ordenadoresFiltrados: Observable<Ordenador[]>;
-  contratoForm: FormGroup;
-
-  ordenadores: Array<OrdenadorGasto>;
-  solicitanteSelect: boolean;
-  ordenadorId: number;
-  supervisorId: number;
-  validar: boolean;
-  fechaSolicitante: string;
-  cargoOrdenador: string;
-  fileDocumento: any;
-  uidDocumento: string;
-  idDocumento: number;
-  cargando_proveedores: boolean = true;
   cargando_supervisores: boolean = true;
   cargando_ordenadores: boolean = true;
+
+  ordenadorId: number;
+  supervisorId: number;
+  cargoOrdenador: string;
+  dependenciaSupervisor: String;
+  fileDocumento: any;
 
   @Input() actaRecibidoId: Number;
   @Output() data: EventEmitter<TransaccionEntrada> = new EventEmitter<TransaccionEntrada>();
 
   constructor(
     private entradasHelper: EntradaHelper,
+    private common: CommonEntradas,
     private pUpManager: PopUpManager,
     private fb: FormBuilder,
-    private nuxeoService: NuxeoService,
+    private documento: GestorDocumentalService,
+    private autenticationService: ImplicitAutenticationService,
     private sanitization: DomSanitizer,
-    private documentoService: DocumentoService,
-    private tercerosHelper: TercerosHelper,
-    private translate: TranslateService) {
-    this.ordenadores = new Array<OrdenadorGasto>();
-    this.solicitanteSelect = false;
+    private translate: TranslateService,
+  ) {
     this.ordenadorId = 0;
     this.supervisorId = 0;
-    this.validar = false;
     this.dependenciaSupervisor = '';
   }
-
 
   ngOnInit() {
     this.soporteForm = this.fb.group({
       soporteCtrl: ['', Validators.required],
-    });
-    this.observacionForm = this.fb.group({
-      observacionCtrl: ['', Validators.nullValidator],
     });
     this.ordenadorForm = this.fb.group({
       ordenadorCtrl: ['', Validators.required],
@@ -82,65 +66,25 @@ export class ElaboracionPropiaComponent implements OnInit {
     this.supervisorForm = this.fb.group({
       supervisorCtrl: ['', Validators.required],
     });
+    this.observacionForm = this.common.formObservaciones;
     this.loadSupervisores();
     this.loadOrdenadores();
   }
 
-  // Métodos para validar campos requeridos en el formulario
-  onSolicitanteSubmit() {
-    this.soporteForm.markAsDirty();
-  }
-
-  loadSolicitantes(): void {
-    this.entradasHelper.getSolicitantes(this.fechaSolicitante).subscribe(res => {
-      while (this.ordenadores.length > 0) {
-        this.ordenadores.pop();
-      }
-      if (res !== null) {
-        for (const index of Object.keys(res.ListaOrdenadores.Ordenadores)) {
-          const ordenador = new OrdenadorGasto;
-          ordenador.NombreOrdenador = res.ListaOrdenadores.Ordenadores[index].NombreOrdenador;
-          ordenador.Id = res.ListaOrdenadores.Ordenadores[index].IdOrdenador;
-          ordenador.RolOrdenadorGasto = res.ListaOrdenadores.Ordenadores[index].CargoOrdenador;
-          this.ordenadores.push(ordenador);
-        }
-      }
-    });
-  }
-
-  changeSolicitante(event) {
-    if (!this.solicitanteSelect) {
-      this.solicitanteSelect = !this.solicitanteSelect;
-    }
-    const date: Date = event;
-    const mes = parseInt(date.getUTCMonth().toString(), 10) + 1;
-    if (mes < 10) {
-      this.fechaSolicitante = date.getFullYear() + '-0' + mes + '-' + date.getDate();
-    } else {
-      this.fechaSolicitante = date.getFullYear() + '-' + mes + '-' + date.getDate();
-    }
-    this.loadSolicitantes();
-  }
-
   changeOrdenador() {
-    this.cargoOrdenador = '';
-    for (const i in this.ordenadores) {
-      if (this.ordenadores[i].NombreOrdenador === this.ordenadorForm.value.ordenadorCtrl) {
-        this.ordenadorId = this.ordenadores[i].Id;
-        this.cargoOrdenador = this.ordenadores[i].RolOrdenadorGasto;
-      }
-    }
+    this.cargoOrdenador = this.ordenadorForm.value.ordenadorCtrl.RolOrdenador;
   }
+
   // -------------------------SUPERVISORES--------------------------------------------------------
   loadSupervisores(): void {
     this.entradasHelper.getSupervisores('supervisor_contrato?limit=-1').subscribe(res => {
       if (Array.isArray(res)) {
         this.Supervisores = res;
         this.supervisoresFiltrados = this.supervisorForm.get('supervisorCtrl').valueChanges
-        .pipe(
-          startWith(''),
-          map(val => typeof val === 'string' ? val : this.muestraSupervisor(val)),
-          map(nombre => this.filtroSupervisores(nombre)),
+          .pipe(
+            startWith(''),
+            map(val => typeof val === 'string' ? val : this.muestraSupervisor(val)),
+            map(nombre => this.filtroSupervisores(nombre)),
           );
         // console.log({supervisores: this.Supervisores});
         this.cargando_supervisores = false;
@@ -169,27 +113,22 @@ export class ElaboracionPropiaComponent implements OnInit {
   }
   filtroSupervisores(nombre: string): Supervisor[] {
     // if (nombre.length >= 4 ) {
-      const valorFiltrado = nombre.toLowerCase();
-      return this.Supervisores.filter(sup => sup.Nombre.toLowerCase().includes(valorFiltrado));
+    const valorFiltrado = nombre.toLowerCase();
+    return this.Supervisores.filter(sup => sup.Nombre.toLowerCase().includes(valorFiltrado));
     // } else return [];
   }
 
   muestraSupervisor(sup: Supervisor): string {
     if (sup.Nombre !== undefined) {
       return sup.Nombre;
-    }else {
+    } else {
       return '';
     }
   }
 
-// -------------------------------------ORDENADORES---------------------------------------------------
-  onSoporteSubmit() {
-    if (this.ordenadorId !== 0) {
-      this.soporteForm.markAsDirty();
-    }
-  }
+  // -------------------------------------ORDENADORES---------------------------------------------------
   loadOrdenadores(): void {
-    this.entradasHelper.getOrdenadores('ordenadores').subscribe( res => {
+    this.entradasHelper.getOrdenadores('ordenadores').subscribe(res => {
       if (Array.isArray(res)) {
         this.Ordenadores = res;
         this.ordenadoresFiltrados = this.ordenadorForm.get('ordenadorCtrl').valueChanges
@@ -205,23 +144,19 @@ export class ElaboracionPropiaComponent implements OnInit {
   }
   filtroOrdenadores(nombre: string): Ordenador[] {
     // if (nombre.length >= 4 ) {
-      const valorFiltrado = nombre.toLowerCase();
-      return this.Ordenadores.filter(sup => sup.NombreOrdenador.toLowerCase().includes(valorFiltrado));
+    const valorFiltrado = nombre.toLowerCase();
+    return this.Ordenadores.filter(sup => sup.NombreOrdenador.toLowerCase().includes(valorFiltrado));
     // } else return [];
   }
 
   muestraOrdenador(ord: Ordenador): string {
     if (ord.NombreOrdenador !== undefined) {
       return ord.NombreOrdenador;
-    }else {
+    } else {
       return '';
     }
   }
-// ---------------------------------FIN ORDENADORES-------------------------------------------------
-  onObservacionSubmit() {
-    this.validar = true;
-  }
-
+  // ---------------------------------FIN ORDENADORES-------------------------------------------------
 
   // MÉTODOS PARA CARGAR SOPORTES
   getSoporte(event) {
@@ -243,53 +178,49 @@ export class ElaboracionPropiaComponent implements OnInit {
     return this.sanitization.bypassSecurityTrustUrl(oldURL);
   }
 
-  postSoporteNuxeo(files) {
-    return new Promise((resolve, reject) => {
-      files.forEach((file) => {
-        file.Id = file.nombre;
-        file.nombre = 'soporte_' + file.IdDocumento + '_entradas';
-        // file.key = file.Id;
-        file.key = 'soporte_' + file.IdDocumento;
-      });
-      this.nuxeoService.getDocumentos$(files, this.documentoService)
-        .subscribe(response => {
-          if (Object.keys(response).length === files.length) {
-            // console.log("response", response);
-            files.forEach((file) => {
-              this.uidDocumento = file.uid;
-              this.idDocumento = response[file.key].Id;
-            });
-            resolve(true);
-          }
-        }, error => {
-          reject(error);
-        });
+  async postFile(file: any) {
+    const files = [];
+    const nombre = await this.autenticationService.getMail();
+    files.push({
+      nombre,
+      key: 'Documento',
+      file,
+      IdDocumento: 53,
     });
+
+    const rolePromise = new Promise<number>((resolve, reject) => {
+      this.documento.uploadFiles(files).subscribe((data: any) => {
+        resolve(+data[0].res.Id);
+      });
+    });
+    return rolePromise;
   }
 
-// Método para enviar registro
+  onObservacionSubmit() {
+    this.pUpManager.showAlertWithOptions(this.common.optionsSubmit)
+      .then((result) => {
+        if (result.value) {
+          this.onSubmit();
+        }
+      });
+  }
+
+  // Método para enviar registro
   async onSubmit() {
-    if (this.validar) {
-      await this.postSoporteNuxeo([this.fileDocumento]);
+    const documento = await this.postFile(this.fileDocumento);
+    const detalle = {
+      acta_recibido_id: +this.actaRecibidoId,
+      supervisor: this.supervisorForm.value.supervisorCtrl.Id,
+      ordenador_gasto_id: this.ordenadorForm.value.ordenadorCtrl.Id,
+    };
 
-      const detalle = {
-        acta_recibido_id: +this.actaRecibidoId,
-        supervisor: this.supervisorForm.value.supervisorCtrl.Id,
-        ordenador_gasto_id: this.ordenadorForm.value.ordenadorCtrl.Id,
-        // solicitante_id: +this.supervisorId,
-      };
+    const transaccion = <TransaccionEntrada>{
+      Observacion: this.observacionForm.value.observacionCtrl,
+      Detalle: detalle,
+      FormatoTipoMovimientoId: 'ENT_EP',
+      SoporteMovimientoId: documento,
+    };
 
-      const transaccion = <TransaccionEntrada>{
-        Observacion: this.observacionForm.value.observacionCtrl,
-        Detalle: detalle,
-        FormatoTipoMovimientoId: 'ENT_EP',
-        SoporteMovimientoId: this.idDocumento,
-      };
-
-      this.data.emit(transaccion);
-    } else {
-      this.pUpManager.showErrorAlert('No ha llenado todos los campos! No es posible hacer el registro.');
-    }
-
+    this.data.emit(transaccion);
   }
 }
