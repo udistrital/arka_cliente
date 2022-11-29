af82bf9f5fd685e41950e7a0cdb645dba46c2748
Ludwing Sanchez <ludwingsanchezleon@gmail.com>
2022-11-28 20:41:06 -0500

    feat #1043: Pruebas seguridad
    - Limpieza @nebular/date-fns

--- src/app/@theme/theme.module.ts
+++ src/app/@theme/theme.module.ts
@@ -114,7 +114,6 @@ import {
   NbTooltipModule,
   NbCalendarKitModule,
 } from '@nebular/theme';
-import { NbDateFnsDateModule } from '@nebular/date-fns';
 import { NbSecurityModule } from '@nebular/security';
 
 import {
@@ -250,8 +249,6 @@ const NB_THEME_PROVIDERS = [
   ...NbChatModule.forRoot({
     messageGoogleMapKey: 'AIzaSyA_wNuCzia92MAmdLRzmqitRGvCF7wCZPY',
   }).providers,
-  ...NbDateFnsDateModule.forRoot({ format: 'dd/MM/yyyy' }).providers,
-  ...NbDateFnsDateModule.forChild({ format: 'dd/MM/yyyy' }).providers,
 ];
 
 @NgModule({
