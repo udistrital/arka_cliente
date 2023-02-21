e772d83fa5271aff8984bb10b3bf1fc5cbae3e78
Ludwing Sanchez <ludwingsanchezleon@gmail.com>
2023-02-21 02:12:52 -0500

    feat #1069: Seguridad
    - Limpieza de entryComponents

--- src/app/pages/bodega-consumo/bodega-consumo-routing.module.ts
+++ src/app/pages/bodega-consumo/bodega-consumo-routing.module.ts
@@ -66,7 +66,6 @@ const routes: Routes = [{
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  entryComponents: [],
 })
 export class BodegaConsumoRoutingModule { }
 
