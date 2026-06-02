2bad154e21000b3672c3bf456d0ae96a6f05a6e6
criss201x <cryteck_1011@hotmail.com>
2026-06-02 16:08:11 -0500

    fix: ajuste linter

--- src/app/pages/ajustes/lista-movimientos/lista-movimientos.component.ts
+++ src/app/pages/ajustes/lista-movimientos/lista-movimientos.component.ts
@@ -82,9 +82,7 @@ export class ListaMovimientosComponent implements OnInit {
     }
     this.salidasHelper.getAjustes().subscribe(res => {
       if (res && res.length) {
-        res = res.filter((ajuste: any) => {
-          // El filtro descarta cualquier movimiento cuyo Detalle.Elementos sea null o undefined antes de iterar, 
-          // evitando el TypeError sin depender del estado de la BD
+        res = res.filter((ajuste: any) => { 
           const detalle = JSON.parse(ajuste.Detalle);
           return detalle.Elementos !== null && detalle.Elementos !== undefined;
         });
